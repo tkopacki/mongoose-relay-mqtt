59263e64542846031cd59371f23c1aa797687f79
Tomasz Kopacki <tomasz@kopacki.eu>
2018-05-21 21:59:06 +0200

    .

--- mjs_fs/api_relay_mqtt.js
+++ mjs_fs/api_relay_mqtt.js
@@ -4,6 +4,7 @@ load('api_mqtt.js');
 function init() {
     for(let idx in Chanels.chanels) {
         let chanel = Chanels.chanels[idx];
+        print("Registering chanel", idx, "on topic", chanel.topic);
         MQTT.sub(chanel.topic, function (connection, topic, message) {
             for(let idx in Chanels.chanels) {
                 let chanel = Chanels.chanels[idx];
