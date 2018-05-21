e52e906e6185d291c80e7c7f509448d517b10764
Tomasz Kopacki <kopacki.tomasz@gmail.com>
2018-05-21 22:18:32 +0200

    Dev (#1)
    .

--- mjs_fs/api_relay_mqtt.js
+++ mjs_fs/api_relay_mqtt.js
@@ -2,20 +2,38 @@ load('api_relay.js');
 load('api_mqtt.js');
 
 function init() {
+    let enabledChanels = Cfg.get('relay.chanels.enabled');
+    if(enabledChanels === undefined) {
+        print('Relay configuration is missing !');
+    } else {
+        let enabledChanelsArray = StringUtils.split(enabledChanels, ',');
+        for(let idx = 0 ; idx < enabledChanelsArray.length ; idx++) {
+            Chanels.get(enabledChanelsArray[idx]).topic = Cfg.get('relay.chanels.' + enabledChanelsArray[idx] + '.topic');
+        }
+    }
+}
+
+function register() {
     for(let idx in Chanels.chanels) {
         let chanel = Chanels.chanels[idx];
+        print("Registering chanel", idx, "on topic", chanel.topic);
         MQTT.sub(chanel.topic, function (connection, topic, message) {
             for(let idx in Chanels.chanels) {
                 let chanel = Chanels.chanels[idx];
                 if(chanel.topic === topic) {
                     print("Recieved new message(", message, ") for chanel", chanel.name);
                     if(message === "1") {
-                        Chanels.on(chanel);
+                        Chanels.on(idx);
                     } else {
-                        Chanels.off(chanel);
+                        Chanels.off(idx);
                     }
                 }
             }
           }, null); 
     }
 }
+
+print("Registering relay on MQTT server...");
+init();
+register();
+print("Relay registration done.");
