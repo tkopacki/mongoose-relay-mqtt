56d1892eaf3618bd131ffcbe7abb45321d873632
Tomasz Kopacki <tomasz@kopacki.eu>
2018-05-30 10:53:11 +0200

    .

--- mjs_fs/api_relay_mqtt.js
+++ mjs_fs/api_relay_mqtt.js
@@ -3,12 +3,12 @@ load('api_mqtt.js');
 
 function init() {
     let enabledchannels = Cfg.get('relay.channels.enabled');
-    if(enabledchannels === undefined) {
+    if (enabledchannels === undefined) {
         print('Relay configuration is missing !');
     } else {
         let enabledchannelsArray = StringUtils.split(enabledchannels, ',');
         print('Enabled channels:', enabledchannelsArray.length);
-        for(let idx = 0 ; idx < enabledchannelsArray.length ; idx++) {
+        for (let idx = 0; idx < enabledchannelsArray.length; idx++) {
             print("Initialiazing channel", idx, "for mqtt transport");
             Channels.get(enabledchannelsArray[idx]).topic = Cfg.get('relay.channels.' + enabledchannelsArray[idx] + '.topic');
             print("MQTT transport for channel", idx, "initialized");
@@ -16,27 +16,43 @@ function init() {
     }
 }
 
-function register() {
-    for(let idx in Channels.channels) {
+function subscribe(connection, topic, message) {
+    for (let idx in Channels.channels) {
+        let channel = Channels.channels[idx];
+        if (channel.topic === topic) {
+            print("Recieved new message(", message, ") for channel", channel.name);
+            sendCallback(topic, message, channel.name);
+            if (message === "1") {
+                Channels.on(idx);
+            } else {
+                Channels.off(idx);
+            }
+        }
+    }
+}
+
+function registerMQTT() {
+    for (let idx in Channels.channels) {
         let channel = Channels.channels[idx];
         print("Registering channel", idx, "on topic", channel.topic);
-        MQTT.sub(channel.topic, function (connection, topic, message) {
-            for(let idx in Channels.channels) {
-                let channel = Channels.channels[idx];
-                if(channel.topic === topic) {
-                    print("Recieved new message(", message, ") for channel", channel.name);
-                    if(message === "1") {
-                        Channels.on(idx);
-                    } else {
-                        Channels.off(idx);
-                    }
-                }
-            }
-          }, null); 
+        MQTT.sub(channel.topic, subscribe, null);
+    }
+}
+
+function sendCallback(topic, message, channelName) {
+    let callbackEnabled = Cfg.get('relay.config.callback.enabled');
+    if (callbackEnabled) {
+        let callbackTopic = Cfg.get('relay.config.callback.topic');
+        MQTT.pub(callbackTopic, {
+            "topic": topic,
+            "message": (message === "1" ? "ON" : "OFF"),
+            "channel": channelName
+        });
+        print("Callback message sent");
     }
 }
 
 print("Registering relay on MQTT server...");
 init();
-register();
+registerMQTT();
 print("Relay registration done.");
