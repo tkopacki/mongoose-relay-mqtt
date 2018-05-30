729f591f7266b5895d136a32859afdf91ca06f97
Tomasz Kopacki <tomasz@kopacki.eu>
2018-05-30 09:51:42 +0200

    .

--- mjs_fs/api_relay_mqtt.js
+++ mjs_fs/api_relay_mqtt.js
@@ -2,30 +2,33 @@ load('api_relay.js');
 load('api_mqtt.js');
 
 function init() {
-    let enabledChanels = Cfg.get('relay.chanels.enabled');
-    if(enabledChanels === undefined) {
+    let enabledchannels = Cfg.get('relay.channels.enabled');
+    if(enabledchannels === undefined) {
         print('Relay configuration is missing !');
     } else {
-        let enabledChanelsArray = StringUtils.split(enabledChanels, ',');
-        for(let idx = 0 ; idx < enabledChanelsArray.length ; idx++) {
-            Chanels.get(enabledChanelsArray[idx]).topic = Cfg.get('relay.chanels.' + enabledChanelsArray[idx] + '.topic');
+        let enabledchannelsArray = StringUtils.split(enabledchannels, ',');
+        print('Enabled channels:', enabledChannelsArray);
+        for(let idx = 0 ; idx < enabledchannelsArray.length ; idx++) {
+            print("Initialiazing channel", idx, "for mqtt transport");
+            channels.get(enabledchannelsArray[idx]).topic = Cfg.get('relay.channels.' + enabledchannelsArray[idx] + '.topic');
+            print("MQTT transport for channel", idx, "initialized");
         }
     }
 }
 
 function register() {
-    for(let idx in Chanels.chanels) {
-        let chanel = Chanels.chanels[idx];
-        print("Registering chanel", idx, "on topic", chanel.topic);
-        MQTT.sub(chanel.topic, function (connection, topic, message) {
-            for(let idx in Chanels.chanels) {
-                let chanel = Chanels.chanels[idx];
-                if(chanel.topic === topic) {
-                    print("Recieved new message(", message, ") for chanel", chanel.name);
+    for(let idx in channels.channels) {
+        let channel = channels.channels[idx];
+        print("Registering channel", idx, "on topic", channel.topic);
+        MQTT.sub(channel.topic, function (connection, topic, message) {
+            for(let idx in channels.channels) {
+                let channel = channels.channels[idx];
+                if(channel.topic === topic) {
+                    print("Recieved new message(", message, ") for channel", channel.name);
                     if(message === "1") {
-                        Chanels.on(idx);
+                        channels.on(idx);
                     } else {
-                        Chanels.off(idx);
+                        channels.off(idx);
                     }
                 }
             }
